5b156aa2703e9ccceca3f00680dbc575aec12f3e
yukittycat <91050701+yukittycat@users.noreply.github.com>
2021-10-29 23:50:08 +0800

    Updated foo.R
    Changes listed in ##

--- foo.R
+++ foo.R
@@ -4,17 +4,25 @@ setwd("C:/Users/E5-471G/Documents/SIM-UOL/2021-22 Year 3/ST2195 Programming for 
 r <- 2
 
 # Function to compute the volume of a sphere with radius r
-volume <- function(r,rho) {
-  3/4*pi*r^2
+volume <- function(r) {
+  4/3*pi*r^3
 }
+
+## removed rho variable as it is redundant
+## changed to accurate sphere volume formula 4/3*pi*r^3 from 3/4*pi*r^2
+
 
 # Function to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r){
-  r <- 22
-  for (r in 2:4){ 
-    volume(r)
+  for (power in 1:3){ ## ie. (r^1)^3, (r^2)^3, (r^3)^3
+    volume(r^power)
+    print(paste("The volume of the sphere with radius", r^power, "cm is", round(volume(r^power),2), "cm^3."))
   }
 }
 
-# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2, R^3
+## volume_vector not printed in browser(), added print(paste())
+## new parameter 'power' ie. radius to the power of 'power'^3
+## removed variable 'r' inside volume_vector such that only 1 'r' as control to prevent miscalculations
+
+# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2, r^3
 volume_vector(r)
